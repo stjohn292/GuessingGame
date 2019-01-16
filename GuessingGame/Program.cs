31a6d5655a021ad50468e87e4d67dcae4cc395cd
Matt <you@example.com>
2019-01-16 11:26:34 -0500

    Added goodbye

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -7,6 +7,7 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye!");
         }
     }
 }
