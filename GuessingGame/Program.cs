939ace852cd54285ebfd8c50c50a25ed3c0c22b2
Matt <you@example.com>
2019-01-16 14:13:04 -0500

    0 option

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,8 +6,31 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Adios!");
+            Console.WriteLine("Guess a number between 1 and 10.");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You guessed: " + userGuess);
+
+            if (userGuess.Equals("7"))
+            {
+                Console.WriteLine("You win!");
+            }
+            if (userGuess.Equals("0"))
+            {
+                Console.WriteLine("I said between 1 and 10. Try again.");
+                userGuess = Console.ReadLine();
+            }
+
+            else
+
+            {
+                Console.WriteLine("You lost.");
+               
+            }
+            
+          
+               
+            
+            
         }
     }
 }
